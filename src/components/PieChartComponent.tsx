6e2a77047e7ea0e2543aba06174e2966b919da9b
karl.hui <karl.hui@cyber-pacer.com>
2024-10-27 19:18:14 -0400

    chore: fix type err

--- src/components/PieChartComponent.tsx
+++ src/components/PieChartComponent.tsx
@@ -31,7 +31,7 @@ const PieChartComponent: React.FC<Props> = ({ fruits }) => {
           outerRadius={120}
           label={({ name }) => name} // Shorter label format
         >
-          {chartData.map((entry, index) => (
+          {chartData.map((_entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
