d87e28b971e3e48c6e68ae714b682380c83ffc6a
karl.hui <karl.hui@cyber-pacer.com>
2024-10-27 20:05:02 -0400

    fix: api to vite file

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { FRUITS_ENDPOINT } from './src/config/index';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: FRUITS_ENDPOINT,
+        target:
+          'https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
