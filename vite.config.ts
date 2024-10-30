fa49ad582501530461bf07415614a307840addf7
karl.hui <karl.hui@cyber-pacer.com>
2024-10-29 23:13:07 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig(() => {
+  // const env = loadEnv(mode, process.cwd());
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
           target:
-            'https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws/',
+            'https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
